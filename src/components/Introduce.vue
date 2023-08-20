7af824f860d7e030087b9fa5ff952d9f35dbe9f3
Yaron Levy <ron5054@gmail.com>
2023-08-20 15:41:07 +0300

    first commit

---
+++ src/components/Introduce.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="introduce-container" id="home">
+        <h1>Hi, I am <span>Yaron</span>, <br> Frontend/Full Stack <br>Developer</h1>
+        <small> I make beatifully simple code and I love what I do.<br>As simple as that!</small>
+    </section>
+</template>
